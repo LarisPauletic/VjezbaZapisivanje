04d17d6c7f229cff6e82ee9ef9e147337baeedd2
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-04-12 09:19:03 +0200

    Add files via upload

---
+++ VjezbaZapisivanje/VjezbaZapisivanje/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace FileKlasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ime, prezime, godRod, email;
+
+            string path = @"C:\Documents\testNIOP\PrezimeImeSystemNiOP.csv";
+            Console.Write("Unesite ime: ");
+             ime =Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+             prezime =Console.ReadLine();
+            Console.Write("Unesite datum rodenja:");
+             godRod = Console.ReadLine();
+            Console.Write("Unesite email:");
+             email = Console.ReadLine();
+
+           
+
+
+
+
+    }
+    }
+}
